7ddf4f6994f407ef0663d06eda301deb09a6d58a
AlbertoSilvEn <aalbertosilvae@gmail.com>
2023-09-27 22:38:43 -0600

    Se trabaja con operadores aritmeticos y se ven los primeros temas de JAVA

---
+++ HolaMundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package holaMundo;
+
+public class HolaMundo {
+	//Escribir el meetodo main
+	public static void main(String []args) {
+		System.out.println("Hola mundo :D");
+	}
+
+} //Fin de mi clase
